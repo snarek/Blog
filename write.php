6c828101fff46c8452a371e7fb83d8f1a438117a
snarek <snarek@yandex.ru>
2016-02-19 16:09:52 +0400

    Verjnakan

--- write.php
+++ write.php
@@ -1,4 +1,25 @@
-<?php require_once ("connect.php");?>
+<?php
+	if (isset($_POST['send'])) {
+		$from=htmlspecialchars($_POST['from']);
+		$email=htmlspecialchars($_POST['email']);
+		$message=htmlspecialchars($_POST['message']);
+		$subject=htmlspecialchars($_POST['subject']);
+		$_SESSION['from']=$from;
+		$_SESSION['email']=$email;
+		$_SESSION['message']=$message;
+		$_SESSION['subject']=$subject;
+		$message = wordwrap($message, 70, "\r\n");
+		$subject="=?UTF-8?B?".base64_encode($subject)."?=";
+		$header = "From:" . $email . "\r\n";
+		$header.= "MIME-Version: 1.0\r\n";
+		$header.= "Content-Type: text/plain; charset=utf-8\r\n";
+		$header.= "X-Priority: 1\r\n";
+		mail("snarek@yandex.ru", $subject, $message, $header);
+		header("Location: success.php?send=1");
+
+		exit;
+	}
+?>
 <!Doctype html>
 <html>
 	<head>
@@ -14,23 +35,52 @@
 				<ul>
 					<li><a href="index.php">Home</a></li>
 					<li><a href="write.php">Write</a></li>
-					<li><a href="">Home</a></li>
+					<li><a href="contact.php">Contact Us</a></li>
 					<li><a href="">Home</a></li>
 				</ul>
 			</aside>
-        <form method='post' id='post'>
-					Name</br>
-          <input type="text" name="name" required></input></br>
-          Surname</br>
-          <input type="text" name="surname" required></input></br>
-          Email</br>
-          <input type="email" name="email" required></input></br>
-					Post</br>
-					<textarea name="post" required></textarea></br>
-					<a href="index.php">
-						<input type="submit" name="submit" value="POST" id='submit'></input>
-					</a>
-        </form>
+				<h2>Contact Us</h2>
+				<form name='feedback' action='' method="post">
+					<label>Name</label><br />
+					<input type="text" name="from" required value="
+					<?php
+						if(isset($_SESSION['from'])){
+							echo $_SESSION['from'];
+						}else{
+							echo '';
+						}
+					?>">
+					</input><br />
+					<label>Email</label><br />
+					<input type="email" name="email" required value="
+					<?php
+						if(isset($_SESSION['email'])){
+							echo $_SESSION['email'];
+						}else{
+							echo '';
+						}
+					?>"></input><br />
+					<label>Subject</label><br />
+					<input type="text" name="subject" required value="
+					<?php
+						if(isset($_SESSION['subject'])){
+							echo $_SESSION['subject'];
+						}else{
+							echo '';
+						}
+					?>"></input><br />
+					<label>Message</label><br />
+					<textarea name="message" cols="30" rows="10" required>
+						<?php
+							if(isset($_SESSION['message'])){
+								echo $_SESSION['message'];
+							}else{
+								echo '';
+							}
+						?>
+					</textarea><br />
+					<input type="submit" name="send" value="Send"></input>
+				</form>
 		</section>
 		<footer>
 			Narek Sargsyan`s blog (c) 2016
